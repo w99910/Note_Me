3f9e08858e010afdbb609f86e7bdd370115f846d
w99910 <65523475+w99910@users.noreply.github.com>
2021-01-19 17:53:09 +0630

    blah blah12

--- resources/views/layouts/guest_layout.blade.php
+++ resources/views/layouts/guest_layout.blade.php
@@ -14,20 +14,11 @@
     <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
     <link rel="stylesheet" href="{{secure_asset('OwlCarousel2-2.3.4/OwlCarousel2-2.3.4/docs/assets/owlcarousel/assets/owl.carousel.min.css')}}">
     <link rel="stylesheet" href="{{secure_asset('OwlCarousel2-2.3.4/OwlCarousel2-2.3.4/docs/assets/owlcarousel/assets/owl.theme.default.min.css')}}">
-    <!-- Scripts -->
-
-{{--    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>--}}
 </head>
 <body class="font-lora sm:overflow-hidden overflow-x-hidden">
-{{--<div class="top-0 text-secondary z-10 text-xl right-0 w-2/12 h-20 absolute flex justify-evenly">--}}
-{{--       <button>Login</button>--}}
-{{--       <button>Sign Up</button>--}}
-{{--</div>--}}
      @yield('content', 'Default Content')
-<script src="{{ asset('js/app.js') }}"></script>
-<script src="{{asset('OwlCarousel2-2.3.4/OwlCarousel2-2.3.4/docs/assets/owlcarousel/owl.carousel.min.js')}}"></script>
+<script src="{{ secure_asset('js/app.js') }}"></script>
+<script src="{{secure_asset('OwlCarousel2-2.3.4/OwlCarousel2-2.3.4/docs/assets/owlcarousel/owl.carousel.min.js')}}"></script>
 @stack('scripts')
-
-
 </body>
 </html>
